7af04798a1799a12eac9f5b090b3feede403e282
Renaaad3 <rinadno@gmail.com>
2024-09-26 17:05:42 +0300

    Initial Commit

---
+++ Gittest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gittest
+//
+//  Created by Renad Alotaibi on 23/03/1446 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
